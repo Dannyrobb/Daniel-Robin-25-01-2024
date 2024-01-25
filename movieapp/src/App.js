5b99c09c226ca650dff7436972cec65c822d753e
Dannyrobb <dannyrobb1997@gmail.com>
2024-01-25 22:48:34 +0200

    app.js and file structure

--- movieapp/src/App.js
+++ movieapp/src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+// App.js
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import MainScreen from "./MainScreen";
+import FavoritesScreen from "./FavoritesScreen";
+import Header from "./Header";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Header />
+
+        <Route path="/" exact component={MainScreen} />
+        <Route path="/favorites" component={FavoritesScreen} />
+      </div>
+    </Router>
   );
 }
 
