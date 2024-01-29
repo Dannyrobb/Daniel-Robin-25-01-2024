67a8ae73977a24edae3abf80f2f066a942ee13fe
Dannyrobb <dannyrobb1997@gmail.com>
2024-01-29 15:58:32 +0200

    rerolled some changes to MovieDetails,movieSlice, Appjs

--- movieapp2/movieapp2/src/App.js
+++ movieapp2/movieapp2/src/App.js
@@ -1,10 +1,14 @@
 import "./App.css";
+import { useEffect } from "react";
 import MovieDetails from "./components/pages/MovieDetails";
 import { Routes, Route } from "react-router-dom";
 import Favorites from "./components/pages/Favorites";
 import ResponsiveAppBar from "./components/common/ResponsiveAppBar";
+import { useDispatch } from "react-redux";
 import { Stack } from "@mui/material";
 function App() {
+  const dispatch = useDispatch();
+
   return (
     <>
       <Stack
--- movieapp2/movieapp2/src/redux/Slices/movieSlice.js
+++ movieapp2/movieapp2/src/redux/Slices/movieSlice.js
@@ -1,6 +1,53 @@
-// src/redux/Slices/movieSlice.js
+// // src/redux/Slices/movieSlice.js
+// import { createSlice } from "@reduxjs/toolkit";
+// import getMovieDetails from "../../api/movieDetails";
+
+// const initialState = {
+//   movieDetails: null,
+//   loading: false,
+//   error: null,
+// };
+
+// const movieSlice = createSlice({
+//   name: "movie",
+//   initialState,
+//   reducers: {
+//     fetchMovieDetailsStart: (state) => {
+//       state.loading = true;
+//       state.error = null;
+//     },
+//     fetchMovieDetailsSuccess: (state, action) => {
+//       state.loading = false;
+//       state.movieDetails = action.payload;
+//     },
+//     fetchMovieDetailsFailure: (state, action) => {
+//       state.loading = false;
+//       state.error = action.payload;
+//     },
+//   },
+// });
+
+// export const { fetchMovieDetailsStart, fetchMovieDetailsSuccess, fetchMovieDetailsFailure } = movieSlice.actions;
+
+// // Thunk action creator
+// export const fetchMovieDetails = (imdbID) => async (dispatch) => {
+//   try {
+//     dispatch(fetchMovieDetailsStart());
+//     const data = await getMovieDetails(imdbID);
+//     dispatch(fetchMovieDetailsSuccess(data)); // Access action.payload, which should be the movie details
+//   } catch (error) {
+//     dispatch(fetchMovieDetailsFailure(error));
+//   }
+// };
+
+// export const selectMovieDetails = (state) => state.movie.movieDetails;
+// export const selectLoading = (state) => state.movie.loading;
+// export const selectError = (state) => state.movie.error;
+
+// export default movieSlice.reducer;
+
 import { createSlice } from "@reduxjs/toolkit";
-import getMovieDetails from "../../api/movieDetails";
+import getCurrentMovie from "../../api/currentMovie";
 
 const initialState = {
   movieDetails: null,
@@ -33,8 +80,9 @@ export const { fetchMovieDetailsStart, fetchMovieDetailsSuccess, fetchMovieDetai
 export const fetchMovieDetails = (imdbID) => async (dispatch) => {
   try {
     dispatch(fetchMovieDetailsStart());
-    const data = await getMovieDetails(imdbID);
-    dispatch(fetchMovieDetailsSuccess(data)); // Access action.payload, which should be the movie details
+    const data = await getCurrentMovie(imdbID);
+    console.log(data);
+    dispatch(fetchMovieDetailsSuccess(data));
   } catch (error) {
     dispatch(fetchMovieDetailsFailure(error));
   }
@@ -43,5 +91,11 @@ export const fetchMovieDetails = (imdbID) => async (dispatch) => {
 export const selectMovieDetails = (state) => state.movie.movieDetails;
 export const selectLoading = (state) => state.movie.loading;
 export const selectError = (state) => state.movie.error;
+
+// Automatically fetch data for a default IMDb ID when the store initializes
+export const fetchDefaultMovieDetails = () => async (dispatch) => {
+  const defaultImdbID = "tt0133093"; // Set your default IMDb ID
+  dispatch(fetchMovieDetails(defaultImdbID));
+};
 
 export default movieSlice.reducer;
